11d3d469142ad8c037fa9f9ecbc8564cdc2f39ac
lzurch3r <97326615+lzurch3r@users.noreply.github.com>
2022-01-07 21:23:36 -0800

    First main.js commit

---
+++ week1/js/main.js
@@ -0,0 +1,19 @@
+function createList() {
+  var list = document.getElementById('table_contents').innerHTML;
+  const links = [
+    {
+      label: "Week1 notes",
+      url: "week1/index.html"
+    }
+  ]
+
+  var newList = "<ol>";
+  for (i = 0; i < links.length; i++) {
+    let label = links[i].label;
+    let url = links[i].url;
+
+    let n = "<li><a href=\'" + url + "\'><" + label + "</a>";
+    newList += n;
+  }
+  newList += "</ol>";
+}
