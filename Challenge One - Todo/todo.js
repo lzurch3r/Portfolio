1819b1101c379a059ec36915a3f59f31caf90b1c
lzurch3r <97326615+lzurch3r@users.noreply.github.com>
2022-02-02 15:09:56 -0800

    created new branch to work with localStorage

--- Challenge One - Todo/todo.js
+++ Challenge One - Todo/todo.js
@@ -8,8 +8,7 @@ class Task {
   completeTask() { this.active = false; }
 };
 
-let taskList = [];
-loadTaskList();
+//let taskList = [];
 
 /**************************
  * ADD NEW TASK
@@ -18,13 +17,19 @@ loadTaskList();
  ***************************/
 function addNewTask(name) {
   if (name) {
-    let newList = taskList;
-    const newTask = new Task(name);
-    newList.unshift(newTask);
+    
+      let newList = [];
+      newList = newList + readTaskList();
+      console.log(newList.length);
+      const newTask = new Task(name);
+      newList.unshift(newTask);
 
-    taskList = newList;
-    displayTaskList(taskList);
-  }
+      saveTaskList(newList);
+      displayTaskList(readTaskList());
+      delete newTask;
+      newTask = null;
+    }
+
   else {
     const message = "Error: invalid task name!";
     console.log(message);
@@ -35,7 +40,7 @@ function addNewTask(name) {
  * Takes a string and a number as parameters and removes a Task
  * object from the taskList array
  **************************/
-function removeTask(taskName,index) {
+/*function removeTask(taskName,index) {
   if (taskName && (index >= 0)) {
     let newList = taskList;
     
@@ -50,34 +55,78 @@ function removeTask(taskName,index) {
         console.log(message);
       }
   }
-}
-function displayTaskList(taskList) {
+}*/
+/***************************************
+ * DISPLAY TASK LIST
+ * Takes an array as a parameter and 
+ ****************************************/
+function displayTaskList(list) {
+  console.log(list.length);
+  
+  {
+    let message = "";
+  for (let i = 0; i < list.length; i++) {
+    message += list[i];
+  }
+  console.log(message);
+  }
   let newHTML = "";
-  if (taskList.length > 0) {
-    for (let i = 0; i < taskList.length; i = i + 1) {
-      if (taskList[i].isActive()) {
+  if (list && (list.length > 0)) {
+    for (let i = 0; i < list.length; i = i + 1) {
+      if (list[i].isActive()) {
       newHTML += `<tr name="task_row"><td name="task_check"></td>` +
                  `<td name="task_name">` +
-                 taskList[i].getTaskName() + `</td><td name="task_remove" onclick="removeTask('` + taskList[i].getTaskName() + `',` + i + `)">X</td></tr>`;
+                 list[i].getTaskName() + `</td><td name="task_remove" onclick="removeTask('` + list[i].getTaskName() + `',` + i + `)">X</td></tr>`;
       }
       //console.log(newHTML);
     }
   }
-  else if (taskList.length === 0) {
+  else if (!list || list.length === 0) {
     newHTML = "<em>There's nothing here!</em>";
   }
   
   document.getElementById('table_tasks').innerHTML = newHTML;
-  clearTaskInput();
+  clearTaskInput();                                           //clears user input
 }
+/*********************************
+ * CLEAR TASK INPUT
+ * Uses a default string parameter and clears the input element
+ **********************************/
 function clearTaskInput(id='input_task') {
   document.getElementById(id).value = null;
 }
-function loadTaskList() {
-  //let taskList = [];
-  //console.log(taskList.length);
-  displayTaskList(taskList);
-}
 
-//createTaskList();
-//addNewTask("New Task");
+function readTaskList() {
+  const nameChallenge = "Challenge One";
+  const taskList = localStorage.getItem(nameChallenge);
+
+  return taskList;
+}
+/********************************
+ * LOAD TASK LIST
+ * Reads from localStorage and displays the task list
+ *********************************/
+function loadTaskList() {
+  if (readTaskList()) {
+    if (readTaskList().length != 0) {
+    console.log(readTaskList().length);
+      displayTaskList(readTaskList());
+}    else {
+    const message = "<em>There's nothing here!</em>";
+    document.getElementById('table_tasks').innerHTML = message;
+  }
+}
+}
+function saveTaskList(list) {
+  const nameChallenge = "Challenge One";
+  const taskList = list;
+  localStorage.setItem(nameChallenge,taskList);
+}
+function clearAll() {
+  const blankList = [];
+  saveTaskList(blankList);
+
+  const message = "List length: " + readTaskList().length;
+  displayTaskList(readTaskList());
+  console.log(message);
+}
