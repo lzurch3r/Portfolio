5f5456319076e0c58ab2b12230e077c8b2eec206
lzurch3r <97326615+lzurch3r@users.noreply.github.com>
2022-02-01 16:51:47 -0800

    updated todo.js

--- Challenge One - Todo/todo.js
+++ Challenge One - Todo/todo.js
@@ -13,23 +13,40 @@ class Task {
     this.active = false;
   }
 };
-
+/**************************
+ * CREATE TASK LIST
+ * Test code for creating a list of tasks on startup
+ ***************************/
 function createTaskList() {
   let firstTask = new Task("First Task");
   let secondTask = new Task("Second Task");
   let thirdTask = new Task("Third Task");
   let taskArray = [firstTask, secondTask, thirdTask];
-  console.log(document.getElementById('table_tasks').innerHTML);
-  let taskList = document.getElementById('table_tasks').innerHTML;
+  const taskList = document.getElementById('table_tasks').innerHTML;
 
   let newHTML = "";
   for (let i = 0; i < taskArray.length; i++) {
-    newHTML = `<tr name="task_row"><td name="task_check"></td>` +
-               `<td name="task_name">` + taskArray[i].getTaskName +
-               `</td><td name="task_remove">X</td></tr>` + newHTML;
+    newHTML =  newHTML + `<tr name="task_row"><td name="task_check"></td>` +
+               `<td name="task_name">` + taskArray[i].getTaskName() +
+               `</td><td name="task_remove">X</td></tr>`;
   }
   
-  taskList = newHTML + taskList;
+  document.getElementById('table_tasks').innerHTML = newHTML + taskList;
+}
+function addNewTask(name) {
+  if (name) {
+    const newTask = new Task(name);
+    const taskList = document.getElementById('table_tasks').innerHTML;
+    let newHTML = "";
+  
+    if (newTask.isActive()) {
+      newHTML += `<tr name="task_row"><td name="task_check"></td>` +
+                 `<td name="task_name">` + newTask.getTaskName() +
+                 `</td><td name="task_remove">X</td></tr>`;
+    }
+    document.getElementById('table_tasks').innerHTML = newHTML + taskList;
+  }
 }
 
-createTaskList();
+//createTaskList();
+//addNewTask("New Task");
