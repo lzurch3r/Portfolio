2fbcea66cf459108a95440077473aa0d5e3de309
lzurch3r <97326615+lzurch3r@users.noreply.github.com>
2022-03-17 19:52:12 -0700

    updated week11/
    Worked on Team Activity Week 11

--- week11/client/week11.js
+++ week11/client/week11.js
@@ -1,6 +1,15 @@
-import { makeRequest } from "./authHelpers";
+import Auth from "./auth.js";
 
-makeRequest('login', 'POST', {
+const myAuth = new Auth();
+
+// myAuth.login(makeRequest('login', 'POST', {password: 'user1', email: 'user1@email.com'}));
+
+/*makeRequest('login', 'POST', {
 password: 'user1',
 email: 'user1@email.com'
+});*/
+
+document.getElementById('submit-button').addEventListener('click', (e) => {
+    e.preventDefault();
+    myAuth.login();
 });
